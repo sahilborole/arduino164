bf2d8f40ea0de286737ee3ca835940bc3a622f73
sahilborole <84867621+sahilborole@users.noreply.github.com>
2026-03-18 16:46:44 +0530

    Add files via upload

---
+++ sketch_mar18a_project_6_rgb.ino
@@ -0,0 +1,20 @@
+int red = 9; 
+int green = 10; 
+int blue = 11; 
+void setup() { 
+pinMode(red, OUTPUT); 
+pinMode(green, OUTPUT); 
+pinMode(blue, OUTPUT); 
+} 
+void loop() { 
+digitalWrite(red, HIGH); 
+digitalWrite(green, LOW); 
+digitalWrite(blue, LOW); 
+delay(1000); 
+digitalWrite(red, LOW); 
+digitalWrite(green, HIGH); 
+delay(1000); 
+digitalWrite(green, LOW); 
+digitalWrite(blue, HIGH); 
+delay(1000); 
+}
